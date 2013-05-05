fed7b41d4924acb438549054fc3f18a92097bcb2
Piet Delport <pjdelport@gmail.com>
2013-05-05 04:31:48 +0200

    Wrap Telephony.Sms subfolders and intents.

--- src/unhide/android/provider/Telephony.java
+++ src/unhide/android/provider/Telephony.java
@@ -18,9 +18,11 @@ package unhide.android.provider;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.BaseColumns;
+import android.telephony.SmsMessage;
 
 import java.lang.reflect.Method;
 
@@ -272,6 +274,380 @@ public final class Telephony {
             return Reflector._invokeStaticUnchecked(Boolean.class, _isOutgoingFolder, messageType);
         }
         public static final Method _isOutgoingFolder = Reflector._method(_cls, "isOutgoingFolder", int.class);
+
+        /**
+         * Contains all text based SMS messages in the SMS app's inbox.
+         */
+        public static final class Inbox implements BaseColumns, TextBasedSmsColumns {
+
+            public static final Class<?> _cls = Reflector._class("android.provider.Telephony$Sms$Inbox");
+
+            /**
+             * The content:// style URL for this table
+             */
+            public static final Uri CONTENT_URI = Reflector._Uri(_cls, "CONTENT_URI");
+
+            /**
+             * The default sort order for this table
+             */
+            public static final String DEFAULT_SORT_ORDER = Reflector._String(_cls, "DEFAULT_SORT_ORDER");
+
+            /**
+             * Add an SMS to the Draft box.
+             *
+             * @param resolver the content resolver to use
+             * @param address the address of the sender
+             * @param body the body of the message
+             * @param subject the psuedo-subject of the message
+             * @param date the timestamp for the message
+             * @param read true if the message has been read, false if not
+             * @return the URI for the new message
+             */
+            public static Uri addMessage(ContentResolver resolver,
+                    String address, String body, String subject, Long date,
+                    boolean read) {
+                return Reflector._invokeStaticUnchecked(Uri.class, _addMessage, resolver,
+                    address, body, subject, date,
+                    read);
+            }
+            public static final Method _addMessage = Reflector._method(_cls, "addMessage", ContentResolver.class,
+                    String.class, String.class, String.class, Long.class,
+                    boolean.class);
+
+        }
+
+        /**
+         * Contains all sent text based SMS messages in the SMS app's.
+         */
+        public static final class Sent implements BaseColumns, TextBasedSmsColumns {
+
+            public static final Class<?> _cls = Reflector._class("android.provider.Telephony$Sms$Sent");
+
+            /**
+             * The content:// style URL for this table
+             */
+            public static final Uri CONTENT_URI = Reflector._Uri(_cls, "CONTENT_URI");
+
+            /**
+             * The default sort order for this table
+             */
+            public static final String DEFAULT_SORT_ORDER = Reflector._String(_cls, "DEFAULT_SORT_ORDER");
+
+            /**
+             * Add an SMS to the Draft box.
+             *
+             * @param resolver the content resolver to use
+             * @param address the address of the sender
+             * @param body the body of the message
+             * @param subject the psuedo-subject of the message
+             * @param date the timestamp for the message
+             * @return the URI for the new message
+             */
+            public static Uri addMessage(ContentResolver resolver,
+                    String address, String body, String subject, Long date) {
+                return Reflector._invokeStaticUnchecked(Uri.class, _addMessage, resolver,
+                    address, body, subject, date);
+            }
+            public static final Method _addMessage = Reflector._method(_cls, "addMessage", ContentResolver.class,
+                    String.class, String.class, String.class, Long.class);
+
+        }
+
+        /**
+         * Contains all sent text based SMS messages in the SMS app's.
+         */
+        public static final class Draft implements BaseColumns, TextBasedSmsColumns {
+
+            public static final Class<?> _cls = Reflector._class("android.provider.Telephony$Sms$Draft");
+
+            /**
+             * The content:// style URL for this table
+             */
+            public static final Uri CONTENT_URI = Reflector._Uri(_cls, "CONTENT_URI");
+
+            /**
+             * The default sort order for this table
+             */
+            public static final String DEFAULT_SORT_ORDER = Reflector._String(_cls, "DEFAULT_SORT_ORDER");
+
+            /**
+             * Add an SMS to the Draft box.
+             *
+             * @param resolver the content resolver to use
+             * @param address the address of the sender
+             * @param body the body of the message
+             * @param subject the psuedo-subject of the message
+             * @param date the timestamp for the message
+             * @return the URI for the new message
+             */
+            public static Uri addMessage(ContentResolver resolver,
+                    String address, String body, String subject, Long date) {
+                return Reflector._invokeStaticUnchecked(Uri.class, _addMessage, resolver,
+                        address, body, subject, date);
+            }
+            public static final Method _addMessage = Reflector._method(_cls, "addMessage", ContentResolver.class,
+                    String.class, String.class, String.class, Long.class);
+
+            /**
+             * Save over an existing draft message.
+             *
+             * @param resolver the content resolver to use
+             * @param uri of existing message
+             * @param body the new body for the draft message
+             * @return true is successful, false otherwise
+             */
+            public static boolean saveMessage(ContentResolver resolver,
+                    Uri uri, String body) {
+                return Reflector._invokeStaticUnchecked(Boolean.class, _addMessage, resolver,
+                    uri, body);
+            }
+            public static final Method _saveMessage = Reflector._method(_cls, "saveMessage", ContentResolver.class,
+                    Uri.class, String.class);
+
+        }
+
+        /**
+         * Contains all pending outgoing text based SMS messages.
+         */
+        public static final class Outbox implements BaseColumns, TextBasedSmsColumns {
+
+            public static final Class<?> _cls = Reflector._class("android.provider.Telephony$Sms$Outbox");
+
+            /**
+             * The content:// style URL for this table
+             */
+            public static final Uri CONTENT_URI = Reflector._Uri(_cls, "CONTENT_URI");
+
+            /**
+             * The default sort order for this table
+             */
+            public static final String DEFAULT_SORT_ORDER = Reflector._String(_cls, "DEFAULT_SORT_ORDER");
+
+            /**
+             * Add an SMS to the Out box.
+             *
+             * @param resolver the content resolver to use
+             * @param address the address of the sender
+             * @param body the body of the message
+             * @param subject the psuedo-subject of the message
+             * @param date the timestamp for the message
+             * @param deliveryReport whether a delivery report was requested for the message
+             * @return the URI for the new message
+             */
+            public static Uri addMessage(ContentResolver resolver,
+                    String address, String body, String subject, Long date,
+                    boolean deliveryReport, long threadId) {
+                return Reflector._invokeStaticUnchecked(Uri.class, _addMessage, resolver,
+                    address, body, subject, date,
+                    deliveryReport, threadId);
+            }
+            public static final Method _addMessage = Reflector._method(_cls, "addMessage", ContentResolver.class,
+                    String.class, String.class, String.class, Long.class,
+                    boolean.class, long.class);
+
+        }
+
+        /**
+         * Contains all sent text-based SMS messages in the SMS app's.
+         */
+        public static final class Conversations
+                implements BaseColumns, TextBasedSmsColumns {
+
+            public static final Class<?> _cls = Reflector._class("android.provider.Telephony$Sms$Conversations");
+
+            /**
+             * The content:// style URL for this table
+             */
+            public static final Uri CONTENT_URI = Reflector._Uri(_cls, "CONTENT_URI");
+
+            /**
+             * The default sort order for this table
+             */
+            public static final String DEFAULT_SORT_ORDER = Reflector._String(_cls, "DEFAULT_SORT_ORDER");
+
+            /**
+             * The first 45 characters of the body of the message
+             * <P>Type: TEXT</P>
+             */
+            public static final String SNIPPET = Reflector._String(_cls, "SNIPPET");
+
+            /**
+             * The number of messages in the conversation
+             * <P>Type: INTEGER</P>
+             */
+            public static final String MESSAGE_COUNT = Reflector._String(_cls, "MESSAGE_COUNT");
+        }
+
+        /**
+         * Contains info about SMS related Intents that are broadcast.
+         */
+        public static final class Intents {
+
+            public static final Class<?> _cls = Reflector._class("android.provider.Telephony$Sms$Intents");
+
+            /**
+             * Set by BroadcastReceiver. Indicates the message was handled
+             * successfully.
+             */
+            public static final int RESULT_SMS_HANDLED = Reflector._int(_cls, "RESULT_SMS_HANDLED", _MISSING);
+
+            /**
+             * Set by BroadcastReceiver. Indicates a generic error while
+             * processing the message.
+             */
+            public static final int RESULT_SMS_GENERIC_ERROR = Reflector._int(_cls, "RESULT_SMS_GENERIC_ERROR", _MISSING);
+
+            /**
+             * Set by BroadcastReceiver. Indicates insufficient memory to store
+             * the message.
+             */
+            public static final int RESULT_SMS_OUT_OF_MEMORY = Reflector._int(_cls, "RESULT_SMS_OUT_OF_MEMORY", _MISSING);
+
+            /**
+             * Set by BroadcastReceiver. Indicates the message, while
+             * possibly valid, is of a format or encoding that is not
+             * supported.
+             */
+            public static final int RESULT_SMS_UNSUPPORTED = Reflector._int(_cls, "RESULT_SMS_UNSUPPORTED", _MISSING);
+
+            /**
+             * Broadcast Action: A new text based SMS message has been received
+             * by the device. The intent will have the following extra
+             * values:</p>
+             *
+             * <ul>
+             *   <li><em>pdus</em> - An Object[] od byte[]s containing the PDUs
+             *   that make up the message.</li>
+             * </ul>
+             *
+             * <p>The extra values can be extracted using
+             * {@link #getMessagesFromIntent(Intent)}.</p>
+             *
+             * <p>If a BroadcastReceiver encounters an error while processing
+             * this intent it should set the result code appropriately.</p>
+             */
+            public static final String SMS_RECEIVED_ACTION =
+                    "android.provider.Telephony.SMS_RECEIVED";
+
+            /**
+             * Broadcast Action: A new data based SMS message has been received
+             * by the device. The intent will have the following extra
+             * values:</p>
+             *
+             * <ul>
+             *   <li><em>pdus</em> - An Object[] of byte[]s containing the PDUs
+             *   that make up the message.</li>
+             * </ul>
+             *
+             * <p>The extra values can be extracted using
+             * {@link #getMessagesFromIntent(Intent)}.</p>
+             *
+             * <p>If a BroadcastReceiver encounters an error while processing
+             * this intent it should set the result code appropriately.</p>
+             */
+            public static final String DATA_SMS_RECEIVED_ACTION = Reflector._String(_cls, "DATA_SMS_RECEIVED_ACTION");
+
+            /**
+             * Broadcast Action: A new WAP PUSH message has been received by the
+             * device. The intent will have the following extra
+             * values:</p>
+             *
+             * <ul>
+             *   <li><em>transactionId (Integer)</em> - The WAP transaction ID</li>
+             *   <li><em>pduType (Integer)</em> - The WAP PDU type</li>
+             *   <li><em>header (byte[])</em> - The header of the message</li>
+             *   <li><em>data (byte[])</em> - The data payload of the message</li>
+             *   <li><em>contentTypeParameters (HashMap&lt;String,String&gt;)</em>
+             *   - Any parameters associated with the content type
+             *   (decoded from the WSP Content-Type header)</li>
+             * </ul>
+             *
+             * <p>If a BroadcastReceiver encounters an error while processing
+             * this intent it should set the result code appropriately.</p>
+             *
+             * <p>The contentTypeParameters extra value is map of content parameters keyed by
+             * their names.</p>
+             *
+             * <p>If any unassigned well-known parameters are encountered, the key of the map will
+             * be 'unassigned/0x...', where '...' is the hex value of the unassigned parameter.  If
+             * a parameter has No-Value the value in the map will be null.</p>
+             */
+            public static final String WAP_PUSH_RECEIVED_ACTION = Reflector._String(_cls, "WAP_PUSH_RECEIVED_ACTION");
+
+            /**
+             * Broadcast Action: A new Cell Broadcast message has been received
+             * by the device. The intent will have the following extra
+             * values:</p>
+             *
+             * <ul>
+             *   <li><em>pdus</em> - An Object[] of byte[]s containing the PDUs
+             *   that make up the message.</li>
+             * </ul>
+             *
+             * <p>The extra values can be extracted using
+             * {@link #getMessagesFromIntent(Intent)}.</p>
+             *
+             * <p>If a BroadcastReceiver encounters an error while processing
+             * this intent it should set the result code appropriately.</p>
+             */
+            public static final String SMS_CB_RECEIVED_ACTION = Reflector._String(_cls, "SMS_CB_RECEIVED_ACTION");
+
+            /**
+             * Broadcast Action: A new Emergency Broadcast message has been received
+             * by the device. The intent will have the following extra
+             * values:</p>
+             *
+             * <ul>
+             *   <li><em>pdus</em> - An Object[] of byte[]s containing the PDUs
+             *   that make up the message.</li>
+             * </ul>
+             *
+             * <p>The extra values can be extracted using
+             * {@link #getMessagesFromIntent(Intent)}.</p>
+             *
+             * <p>If a BroadcastReceiver encounters an error while processing
+             * this intent it should set the result code appropriately.</p>
+             */
+            public static final String SMS_EMERGENCY_CB_RECEIVED_ACTION = Reflector._String(_cls, "SMS_EMERGENCY_CB_RECEIVED_ACTION");
+
+            /**
+             * Broadcast Action: The SIM storage for SMS messages is full.  If
+             * space is not freed, messages targeted for the SIM (class 2) may
+             * not be saved.
+             */
+            public static final String SIM_FULL_ACTION = Reflector._String(_cls, "SIM_FULL_ACTION");
+
+            /**
+             * Broadcast Action: An incoming SMS has been rejected by the
+             * telephony framework.  This intent is sent in lieu of any
+             * of the RECEIVED_ACTION intents.  The intent will have the
+             * following extra value:</p>
+             *
+             * <ul>
+             *   <li><em>result</em> - An int result code, eg,
+             *   <code>{@link #RESULT_SMS_OUT_OF_MEMORY}</code>,
+             *   indicating the error returned to the network.</li>
+             * </ul>
+
+             */
+            public static final String SMS_REJECTED_ACTION = Reflector._String(_cls, "SMS_REJECTED_ACTION");
+
+            /**
+             * Read the PDUs out of an {@link #SMS_RECEIVED_ACTION} or a
+             * {@link #DATA_SMS_RECEIVED_ACTION} intent.
+             *
+             * @param intent the intent to read from
+             * @return an array of SmsMessages for the PDUs
+             */
+            public static SmsMessage[] getMessagesFromIntent(
+                    Intent intent) {
+                return Reflector._invokeStaticUnchecked(SmsMessage[].class, _getMessagesFromIntent,
+                        intent);
+            }
+            public static final Method _getMessagesFromIntent = Reflector._method(_cls, "getMessagesFromIntent",
+                    Intent.class);
+
+        }
 
     }
 
--- tests/src/unhide/android/provider/TelephonyTest.java
+++ tests/src/unhide/android/provider/TelephonyTest.java
@@ -84,4 +84,75 @@ public class TelephonyTest extends TestCase {
         assertNotNull(Telephony.Sms._moveMessageToFolder);
     }
 
+    public void testSmsInbox() {
+        assertNotNull(Telephony.Sms.Inbox._cls);
+
+        assertNotNull(Telephony.Sms.Inbox.CONTENT_URI);
+        assertNotNull(Telephony.Sms.Inbox.DEFAULT_SORT_ORDER);
+
+        // Telephony.Sms.Inbox.addMessage()
+        assertNotNull(Telephony.Sms.Inbox._addMessage);
+    }
+
+    public void testSmsSent() {
+        assertNotNull(Telephony.Sms.Sent._cls);
+
+        assertNotNull(Telephony.Sms.Sent.CONTENT_URI);
+        assertNotNull(Telephony.Sms.Sent.DEFAULT_SORT_ORDER);
+
+        // Telephony.Sms.Sent.addMessage()
+        assertNotNull(Telephony.Sms.Sent._addMessage);
+    }
+
+    public void testSmsDraft() {
+        assertNotNull(Telephony.Sms.Draft._cls);
+
+        assertNotNull(Telephony.Sms.Draft.CONTENT_URI);
+        assertNotNull(Telephony.Sms.Draft.DEFAULT_SORT_ORDER);
+
+        // Telephony.Sms.Draft.addMessage()
+        assertNotNull(Telephony.Sms.Draft._addMessage);
+        // Telephony.Sms.Draft.saveMessage()
+        assertNotNull(Telephony.Sms.Draft._saveMessage);
+    }
+
+    public void testSmsOutbox() {
+        assertNotNull(Telephony.Sms.Outbox._cls);
+
+        assertNotNull(Telephony.Sms.Outbox.CONTENT_URI);
+        assertNotNull(Telephony.Sms.Outbox.DEFAULT_SORT_ORDER);
+
+        // Telephony.Sms.Outbox.addMessage()
+        assertNotNull(Telephony.Sms.Outbox._addMessage);
+    }
+
+    public void testSmsConversations() {
+        assertNotNull(Telephony.Sms.Conversations._cls);
+
+        assertNotNull(Telephony.Sms.Conversations.CONTENT_URI);
+        assertNotNull(Telephony.Sms.Conversations.DEFAULT_SORT_ORDER);
+        assertNotNull(Telephony.Sms.Conversations.SNIPPET);
+        assertNotNull(Telephony.Sms.Conversations.MESSAGE_COUNT);
+    }
+
+    public void testSmsIntents() {
+        assertNotNull(Telephony.Sms.Intents._cls);
+
+        MoreAsserts.assertNotEqual(Telephony._MISSING, Telephony.Sms.Intents.RESULT_SMS_HANDLED);
+        MoreAsserts.assertNotEqual(Telephony._MISSING, Telephony.Sms.Intents.RESULT_SMS_GENERIC_ERROR);
+        MoreAsserts.assertNotEqual(Telephony._MISSING, Telephony.Sms.Intents.RESULT_SMS_OUT_OF_MEMORY);
+        MoreAsserts.assertNotEqual(Telephony._MISSING, Telephony.Sms.Intents.RESULT_SMS_UNSUPPORTED);
+
+        assertNotNull(Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
+        assertNotNull(Telephony.Sms.Intents.DATA_SMS_RECEIVED_ACTION);
+        assertNotNull(Telephony.Sms.Intents.WAP_PUSH_RECEIVED_ACTION);
+        assertNotNull(Telephony.Sms.Intents.SMS_CB_RECEIVED_ACTION);
+        assertNotNull(Telephony.Sms.Intents.SMS_EMERGENCY_CB_RECEIVED_ACTION);
+        assertNotNull(Telephony.Sms.Intents.SIM_FULL_ACTION);
+        assertNotNull(Telephony.Sms.Intents.SMS_REJECTED_ACTION);
+
+        // Telephony.Sms.Intents.getMessagesFromIntent()
+        assertNotNull(Telephony.Sms.Intents._getMessagesFromIntent);
+    }
+
 }
